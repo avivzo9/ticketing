146d24c9ffb6c49822861fda2c2b273e1bab7e1e
Aviv Zohar <avivzo9@gmail.com>
2024-07-21 20:53:36 +0300

    Section 10

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+// routers
+import { currentUserRouter } from './routes/currentUser';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+// middlewares
+import { errorHandler } from './middlewares/errorHandler';
+import { NotFoundError } from './errors/notFoundError';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}))
+
+// use routers
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError();
+})
+
+// middlewares
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-// routers
-import { currentUserRouter } from './routes/currentUser';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-// middlewares
-import { errorHandler } from './middlewares/errorHandler';
-import { NotFoundError } from './errors/notFoundError';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-
-// use routers
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-    throw new NotFoundError();
-})
-
-// middlewares
-app.use(errorHandler);
+import mongoose from "mongoose";
+import { app } from "./app";
 
 const init = async () => {
     if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
