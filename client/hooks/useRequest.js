4e6968891333a6ea9428f6e41d3458b0d2371244
Aviv Zohar <avivzo9@gmail.com>
2024-07-29 21:24:18 +0300

    Half of section 11

---
+++ client/hooks/useRequest.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export default ({ url, method, body, onSuccess }) => {
+    const [errors, setErrors] = useState([]);
+
+    const doRequest = async () => {
+        setErrors([]);
+
+        try {
+            const res = await axios[method](url, body);
+
+            if (onSuccess) onSuccess(res.data);
+
+            return res.data;
+        } catch (err) {
+            setErrors(<div className="alert alert-danger">
+                <h4>Oops....</h4>
+                <ul className="my-0">
+                    {err?.response?.data?.errors?.map(err => <li key={err.message}>{err.message}</li>)}
+                </ul>
+            </div>);
+        }
+    }
+
+    return { doRequest, errors };
+};
