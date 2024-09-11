9c84f2cd6f32e7223f965f94c5d8bd1e026b2bb9
Aviv Zohar <avivzo9@gmail.com>
2024-09-11 11:28:26 +0300

    Add tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const init = async () => {
+    if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
+
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to mongoDB');
+    } catch (err) {
+        console.log('init', err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Auth listening on port 3000');
+    });
+}
+
+init();
