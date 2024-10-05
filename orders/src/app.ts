ccb18573d9692da0a89393145fe6a048c3e89a76
avivzo9 <avivzo9@gmail.com>
2024-10-05 15:57:01 +0300

    Orders service created

---
+++ orders/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+// routes
+import { createOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
+
+// middlewares
+import { currentUser, errorHandler, NotFoundError } from '@avzticketing/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}));
+
+app.use(currentUser);
+
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
+app.use(createOrderRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError();
+})
+
+// middlewares
+app.use(errorHandler);
+
+export { app };
